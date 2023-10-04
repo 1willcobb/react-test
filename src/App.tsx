418dc9d54afccc2d0b0cd5b9941d09620426f2d3
1willcobb <cobb.will@gmail.com>
2023-10-04 08:09:39 -0700

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
 import "./App.css";
 import React, { useState } from "react";
 import NavBar from "./components/NavBar/NavBar";
+import produce from "immer";
 
 const App = () => {
-  const [person, setPerson] = useState({
-    firstName: "Will",
-    lastName: "Cobb",
-  });
+  const [bug, setBugs] = useState([
+    { id: 1, title: "bug 1", fixed: false },
+    { id: 2, title: "bug 2", fixed: false },
+  ]);
 
   const handleClick = () => {
-    person.lastName === "Cobb"
-      ? setPerson({ ...person, lastName: "Smith" })
-      : setPerson({ ...person, lastName: "Cobb" });
+    // setBugs(bug.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
+    setBugs(
+      produce((draft) => {
+        const bug = draft.find((bug) => bug.id === 1);
+        if (bug) bug.fixed = true;
+      })
+    );
   };
 
   return (
     <>
       <NavBar />
       <div>
-        {person.firstName} {person.lastName}
+        {bug.map((bug) => (
+          <p key={bug.id}>
+            {bug.title} {bug.fixed ? "fixed" : "new"}{" "}
+          </p>
+        ))}
       </div>
       <button onClick={handleClick}>toggle last name</button>
     </>
